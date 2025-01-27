23d36459be0932f1096f45846461138005453843
Evelin Todorov <evelintodo@gmail.com>
2025-01-27 17:38:00 +0200

    Completed getComments method; Added date format validation; fixed errors in creating_comments_table script

--- models/comments.php
+++ models/comments.php
@@ -11,8 +11,18 @@ class Comments
         $this->db = new Database();
     }
 
+    function validateDate($date, $format = 'Y-m-d H:i:s') { 
+        $d = DateTime::createFromFormat($format, $date); 
+        return $d && $d->format($format) === $date; 
+    } 
+
     public function storeComment($song_version_id, $author_name, $timestamp, $content)
     {
+        if($this->validateDate($timestamp) == false)
+        {
+            throw new Exception("Invalid timestamp format passed for storing in the database!");
+        }
+
         $sql = "INSERT INTO comments(song_version_id, author, upload_timestamp, content) VALUES (:song_version_id, :author, :upload_timestamp, :content)";
         $this->db->query($sql);
 
@@ -24,14 +34,14 @@ class Comments
         return $this->db->execute();
     }
 
-    public function retrieveComment($song_version_id)
+    public function getComments($song_version_id)
     {
         $sql = "SELECT author, upload_timestamp, content FROM comments WHERE song_version_id=:song_version_id";
         $this->db->query($sql);
 
         $this->db->bind(":song_version_id", $song_version_id);
 
-        $result = $this->db->getAllResults();
+        $result = $this->db->fetchAllResults();
         return $result;
     }
 }
