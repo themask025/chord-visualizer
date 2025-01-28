7423ceda37fffc2b75125df8358999ee05be8de8
toduko <teddyepich@gmail.com>
2025-01-28 17:37:22 +0200

    Added song search results

--- controllers/song_controller.php
+++ controllers/song_controller.php
@@ -4,10 +4,12 @@ require_once "controller.php";
 class SongController extends Controller
 {
     private $song_model;
+    private $version_model;
 
     public function __construct()
     {
         $this->song_model = $this->loadModel('Song');
+        $this->version_model = $this->loadModel('Version');
     }
 
     public function findSong()
@@ -15,13 +17,26 @@ class SongController extends Controller
         if ($_SERVER['REQUEST_METHOD'] === 'GET')
         {
             $songs = $this->song_model->filterSongsByName($_GET["song_name"]);
-            $data = ["songs" => $songs];
+            foreach ($songs as $key => $song)
+            {
+                $version_count = count($this->version_model->getVersionsNameAuthorBySongId($song["id"]));
+                if ($version_count === 1) {
+                  $version_count = "1 version"; 
+                } else {
+                  $version_count = "{$version_count} versions";
+                }
+                $search_results[$key]["href"] = "/chord-visualizer/version/searchSongVersions?song_id={$song["id"]}";
+                $search_results[$key]["main"] = $song["title"];
+                $search_results[$key]["sub"] = $song["performer"];
+                $search_results[$key]["count"] = $version_count;
+            }
+            $data  = ["search_query" => 'Search results for "'.$_GET["song_name"].'"', "search_results" => $search_results];
+            $this->renderView('search_results', $data);
         }
-        $this->renderView('found_songs', $data);
     }
     public function index()
     {
-        $this->renderView('songs');
+      header("Location: /chord-visualizer/");
     }
 
 }
