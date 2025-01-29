3c24f9ba274041b2e571108c620883020c160fb3
toduko <teddyepich@gmail.com>
2025-01-29 12:38:48 +0200

    Fixed search results styling

--- views/search_results/search_results.php
+++ views/search_results/search_results.php
@@ -9,11 +9,9 @@ if (isset($data)) {
     echo "<div class=\"results-list\">";
     foreach ($data["search_results"] as $search_result) {
         echo "<a class=\"results-item\" href=\"{$search_result["href"]}\">";
-        echo "<span class=\"results-item-span-main\">{$search_result["main"]}";
-        echo " ";
-        echo "<span class=\"results-item-span-sub\">{$search_result["sub"]}</span></span>";
-        echo " ";
+        echo "<span class=\"results-item-span-main\">{$search_result["main"]}</span>";
         echo "<span class=\"results-item-span-count\">{$search_result["count"]}</span>";
+        echo "<span class=\"results-item-span-sub\">{$search_result["sub"]}</span>";
         echo "</a>";
     }
     echo "</div>";
