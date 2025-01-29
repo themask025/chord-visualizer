2a8fa654c69e8762d0f0e233a5eda923f2acf721
Evelin Todorov <evelintodo@gmail.com>
2025-01-29 12:41:24 +0200

    Working prototype of the designs

--- views/home/index.php
+++ views/home/index.php
@@ -36,12 +36,15 @@ require_once(__DIR__ . "/../../constants.php");
         ?>
         <div class="landing-page-title-container">
             <h1 class="landing-page-title">Welcome to </h1>
-            <h1>CHORD VISUALIZER</h1>
-            <img src="<?php echo BASE_PATH; ?>assets/landing_page_title_notes.png" class="landing-page-title-image" alt="Notes">
+            <h1 class="landing-page-title-logo">CHORD</h1>
+            <h1 class="landing-page-title-logo">VISUALIZER</h1>
+            <div class="title-image-wrapper">
+                <img src="<?php echo BASE_PATH; ?>assets/landing_page_title_notes.png" class="landing-page-title-image" alt="Notes">
+            </div>
         </div>
 
         <form class="song-search" action="/chord-visualizer/song/findSong" method="GET">
-            <input type="text" name="song_name" placeholder="Search for a song by its name">
+            <input type="text" name="song_name" class="song-searchbar" placeholder="Search for a song">
             <input type="submit" hidden>
         </form>
 
@@ -55,15 +58,14 @@ require_once(__DIR__ . "/../../constants.php");
             echo '<button class="close-button" onclick="set_form_hidden(true)">X</button>';
             echo '<input class="form-input" type="text" placeholder="Enter song name" id="song_name" name="song_name" required>';
             echo '<input class="form-input" type="text" placeholder="Enter song author" id="song_author" name="song_author" required>';
-            echo '<button class="submit-button" type="submit">Add song</button>';
+            echo '<button class="submit-button" type="submit">To Editor</button>';
             echo '</form>';
             echo '</div>';
             echo '</div>';
-        }
-        else
+        } else
             echo '</div>';
         ?>
-    
+
 </body>
 
 </html>
--- views/navigation_bar/index.php
+++ views/navigation_bar/index.php
@@ -10,31 +10,26 @@ require_once(__DIR__ . "/../../constants.php");
     <link rel="stylesheet" href="<?php echo BASE_PATH; ?>views/navigation_bar/styles.css">
 </head>
 
-<body>
+<nav class="navbar">
+    <a href="/chord-visualizer/" class="logo">CHORD VISUALIZER</a>
 
-    <nav class="navbar">
-        <a href="/chord-visualizer/" class="logo">CHORD VISUALIZER</a>
+    <?php
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
 
-        <?php
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
-
-        if (isset($_SESSION["user_id"])) {
-        ?>
-            <form action="<?php echo BASE_PATH; ?>login/logout" method="post" class="logout_form">
-                <input type="text" name="sender" value="<?php echo $_SERVER['REQUEST_URI']; ?>" hidden>
-                <label for="logout_button" class="username"><?php echo $_SESSION["username"]; ?></label>
-                <button type="submit" id="logout_button">Log out</button>
-            </form>
-        <?php
-        } else {
-        ?>
-            <a href="/chord-visualizer/login" class="login_button", type>Log in</a>";
-        <?php
-        }
-        ?>
-    </nav>
-
-
-</body>
+    if (isset($_SESSION["user_id"])) {
+    ?>
+        <form action="<?php echo BASE_PATH; ?>login/logout" method="post" class="logout-form">
+            <input type="text" name="sender" value="<?php echo $_SERVER['REQUEST_URI']; ?>" hidden/>
+            <label for="logout-button" class="username"><?php echo $_SESSION["username"]; ?></label>
+            <button type="submit" id="logout-button" class="logout-button"/></button>
+        </form>
+    <?php
+    } else {
+    ?>
+        <a href="/chord-visualizer/login" class="login-button"><img src="<?php echo BASE_PATH; ?>assets/icon_log_in.png" alt="Log in icon"></a>
+    <?php
+    }
+    ?>
+</nav>
